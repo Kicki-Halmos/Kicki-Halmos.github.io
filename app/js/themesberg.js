1029c258b447ff3e02493c5c580282bb7e194e2b
Kicki Halmos <kickihalmos@gmail.com>
2020-11-05 09:04:21 +0100

    footer och cv fortf inte bra

--- app/js/themesberg.js
+++ app/js/themesberg.js
@@ -1,55 +1,69 @@
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
     let portfolio = document.getElementById("portfolio");
     let portfolio_accordion = document.getElementById("portfolio_accordion");
-    let grid = document.getElementById("grid");
+    portfolio_accordion.style.setProperty("display", "none");
 
-    portfolio.addEventListener("click", function(e){
-        console.log(portfolio_accordion);
-        console.log(portfolio);
-      
-            portfolio.classList.toggle("animation");
-            if(portfolio.classList.contains("animation")){
+    let music = document.getElementById("music");
+    let music_accordion = document.getElementById("music_accordion");
+    music_accordion.style.setProperty("display", "none");
+
+    let cv = document.getElementById("cv");
+    let cv_accordion = document.getElementById("cv_accordion");
+    cv_accordion.style.setProperty("display", "none");
+    portfolio.addEventListener("click", function (e) {
+
+        portfolio.classList.toggle("animation");
+        if (portfolio.classList.contains("animation")) {
             portfolio_accordion.classList.add("open");
             portfolio_accordion.style.removeProperty("display", "none");
             portfolio_accordion.classList.remove("close");
-           
-            }
 
-            else{
-                portfolio_accordion.classList.add("close");
-                portfolio_accordion.classList.remove("open");
-                portfolio_accordion.style.setProperty("display", "none")
-            }
-      
-       
+        } else {
+            portfolio_accordion.classList.add("close");
+            portfolio_accordion.classList.remove("open");
+            portfolio_accordion.style.setProperty("display", "none")
+        }
+
+
     });
 
-    let music = document.getElementById("music");
+    music.addEventListener("click", function (e) {
+        console.log(music);
 
-    music.addEventListener("click", function(e){
-        if (music.classList.contains("animation")){
-            music.classList.remove("animation");
-           
+        music.classList.toggle("animation");
+        if (music.classList.contains("animation")) {
+            music_accordion.classList.add("open");
+            music_accordion.style.removeProperty("display", "none");
+            music_accordion.classList.remove("close");
+
+        } else {
+            music_accordion.classList.add("close");
+            music_accordion.classList.remove("open");
+            music_accordion.style.setProperty("display", "none")
         }
-        else{
-            music.classList.add("animation");
-        }
-       
+
+
     });
 
-    let cv = document.getElementById("cv");
+    cv.addEventListener("click", function (e) {
 
-    cv.addEventListener("click", function(e){
-        if (cv.classList.contains("animation")){
-            cv.classList.remove("animation");
-           
+        cv.classList.toggle("animation");
+        if (cv.classList.contains("animation")) {
+            cv_accordion.classList.add("open");
+            cv_accordion.style.removeProperty("display", "none");
+            cv_accordion.classList.remove("close");
+
+        } else {
+            cv_accordion.classList.add("close");
+            cv_accordion.classList.remove("open");
+            cv_accordion.style.setProperty("display", "none")
         }
-        else{
-            cv.classList.add("animation");
-        }
-       
+
+
     });
 
-    
+
+
+
 
 });
